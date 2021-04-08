361b043e31b60315b07c54a86e34a91e5795561c
Crew Spence <crewspence@gCrewSpence.lan>
2021-04-08 14:18:31 -0500

    Wrote removeEdge method.

--- udemy_graphs.js
+++ udemy_graphs.js
@@ -12,4 +12,24 @@ class Graph {
     addVertex(vertexName) {
         this.adjacencyList[vertexName] = [];
     }
+    
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    removeEdge(vertex1, vertex2) {
+        const filter = (array, value) => {
+            let newArr = [];
+            for(let i = 0; i < array.length; i++) {
+                if(array[i] !== value) {
+                    newArr.push(array[i]);
+                }
+            }
+            return newArr;
+        }
+
+        this.adjacencyList[vertex1] = filter(this.adjacencyList[vertex1], vertex2);
+        this.adjacencyList[vertex2] = filter(this.adjacencyList[vertex2], vertex1);
+    }
 }
